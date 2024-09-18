0c53f251b0d0cd919e9526247f07cf699c758c08
fslse <3455403891@qq.com>
2024-09-18 11:38:00 +0800

    加载dll - 代码调整

--- UnityProject/Assets/Scripts/Fire/Startup/LoadDLL.cs
+++ UnityProject/Assets/Scripts/Fire/Startup/LoadDLL.cs
@@ -18,19 +18,10 @@ namespace Scripts.Fire.Startup
         public override void Start()
         {
             base.Start();
-            Startup().Forget();
-        }
-
-        private async UniTaskVoid Startup()
-        {
-            await Execute();
-            GameLog.LogDebug("Try run App.Main");
-            RunAppMain(); // 启动启动 全部启动
-            workflow.OnTaskFinished(this);
+            Execute().Forget();
         }
 
         private System.Reflection.Assembly mainAssembly;
-
 
         private async UniTask Execute()
         {
@@ -39,48 +30,42 @@ namespace Scripts.Fire.Startup
 #else
             // / 实机
             var ab = await AssetManager.Instance.LoadAssetBundle("scripts.ab");
-            GameLog.LogDebug("DLL AB", ZString.Join("\n", ab.GetAllAssetNames()));
+            GameLog.LogWarning("DLL AB\n", ZString.Join("\n", ab.GetAllAssetNames()));
 
+            // 加载热更新dll
+            // 如果有多个热更新dll，按照依赖顺序加载，先加载被依赖的assembly
             var dllBytes = ab.LoadAsset<TextAsset>("Assembly-CSharp.dll.bytes").bytes;
             mainAssembly = System.Reflection.Assembly.Load(dllBytes);
 
-            LoadMetadataForAOTAssembly();
-            return;
-
-            unsafe void LoadMetadataForAOTAssembly()
+            // 为 AOT DLL 补充元数据
+            foreach (var assembly in AOTGenericReferences.PatchedAOTAssemblyList)
             {
-                foreach (var assembly in AOTGenericReferences.PatchedAOTAssemblyList)
+                try
                 {
-                    try
-                    {
-                        dllBytes = ab.LoadAsset<TextAsset>(assembly.Replace("dll", "bytes")).bytes;
-                    }
-                    catch (Exception e)
-                    {
-                        GameLog.LogError("Load AOT DLL", $"{assembly.Replace("dll", "bytes")}\n{e.Message}");
-                        continue;
-                    }
-
-                    fixed (byte* ptr = dllBytes)
-                    {
-                        // HomologousImageMode.SuperSet: This mode relaxes the requirements for AOT dll, you can use either the cut AOT dll or the original AOT dll.
-                        LoadImageErrorCode errorCode = RuntimeApi.LoadMetadataForAOTAssembly(dllBytes, HomologousImageMode.SuperSet);
-                        GameLog.LogDebug("LoadMetadataForAOTAssembly", $"{assembly}\nErrorCode : {errorCode}");
-                    }
+                    dllBytes = ab.LoadAsset<TextAsset>(assembly.Replace("dll", "bytes")).bytes;
                 }
+                catch (Exception e)
+                {
+                    GameLog.LogError("Failed to Load Metadata For AOTAssembly", $"{assembly.Replace("dll", "bytes")}\n{e.Message}");
+                    continue;
+                }
+
+                // HomologousImageMode.SuperSet: This mode relaxes the requirements for AOT dll, you can use either the cut AOT dll or the original AOT dll.
+                // 加载assembly对应的dll，会自动为它hook。一旦aot泛型函数的native函数不存在，用解释器版本代码
+                LoadImageErrorCode errorCode = RuntimeApi.LoadMetadataForAOTAssembly(dllBytes, HomologousImageMode.SuperSet);
+                GameLog.LogDebug("LoadMetadataForAOTAssembly", $"{assembly}\nErrorCode : {errorCode}");
             }
 #endif
-        }
 
-        private void RunAppMain()
-        {
+            // 热更新程序集入口
             try
             {
                 mainAssembly!.GetType("HotFix.App")!.GetMethod("Main")!.Invoke(null, null);
+                workflow.OnTaskFinished(this);
             }
             catch (Exception e)
             {
-                GameLog.LogError("Run App Main", e.Message);
+                GameLog.LogError("Failed to run HotFix.App.Main", e.Message);
             }
         }
     }
